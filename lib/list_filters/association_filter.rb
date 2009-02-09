78518e7b050aff77e914c8a6a6cd70dc0346b0bb
Tys von Gaza <tys@tvg.ca>
2009-02-09 14:55:33 -0700

    fix for association filter

--- lib/list_filters/association_filter.rb
+++ lib/list_filters/association_filter.rb
@@ -3,26 +3,30 @@ class ListFilters::AssociationFilter < ActiveScaffold::DataStructures::ListFilte
   # Return a list of conditions based on the params 
   def find_options
     begin
-      assocation = @core.model.reflect_on_association(assocation_name)
+      association = association_class
       options = {}
-      options[:include] = assocation_name
-      options[:conditions] = ["#{assocation.klass.table_name}.id IN (?)", params]
+      options[:include] = association_name
+      options[:conditions] = ["#{association.klass.table_name}.id IN (?)", params]
 
-      return options           
+      return options      
   	end unless params.nil? || params.empty?
   end
   
   def verbose
     begin
-  	  ar_class = association_tree.last.class_name.constantize
+  	  ar_class = association_class
     	associated_values = ar_class.find(params).sort {|a,b| a.to_label <=> b.to_label }
     	verbose_values = associated_values.collect{|av| av.to_label}.join(", ")
     	return verbose_values
     end unless params.nil? || params.empty?
   end
   
-  def assocation_name
+  def association_name
     @options[:association] || @name
   end
-	
+  
+  def association_class
+    association = @core.model.reflect_on_association(association_name)
+    association.klass
+  end
 end
