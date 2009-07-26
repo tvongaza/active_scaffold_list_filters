1b08d43ed329c41714963065ae21e0f1e4531bdd
devrandom <c1.github@niftybox.net>
2009-07-26 11:21:29 +0800

    put helpers where they belong in directory hierarchy

--- environment.rb
+++ environment.rb
@@ -1,6 +1,9 @@
 # load all our files, eventually figure out how to get these to load by themselves by convention
 $:.unshift(File.dirname(__FILE__))
 
+require "#{File.dirname(__FILE__)}/lib/helpers/view_helpers.rb"
+require "#{File.dirname(__FILE__)}/lib/active_scaffold/helpers/view_helpers.rb"
+
 ActiveScaffold::Config::Core.send(:include, Extensions::Core)
 ActionView::Base.send(:include, Helpers::ViewHelpers)
 
