2ad0603fef618ce9bb3adaef52475b616da79a8a
devrandom <c1.github@niftybox.net>
2009-07-26 13:17:20 +0800

    New filter: :record_select_association - based on the excellent RecordSelect plugin

---
+++ lib/list_filters/record_select_association_filter.rb
@@ -0,0 +1,32 @@
+class ListFilters::RecordSelectAssociationFilter < ActiveScaffold::DataStructures::ListFilter
+
+  # Return a list of conditions based on the params 
+  def find_options
+    begin
+      association = association_class
+      options = {}
+      options[:include] = association_name
+      options[:conditions] = ["#{association.table_name}.id IN (?)", params]
+
+      return options      
+    end unless params.nil? || params.empty?
+  end
+
+  def verbose
+    begin
+      ar_class = association_class
+      associated_values = ar_class.find(params).sort {|a,b| a.to_label <=> b.to_label }
+      verbose_values = associated_values.collect{|av| av.to_label}.join(", ")
+      return verbose_values
+    end unless params.nil? || params.empty?
+  end
+
+  def association_name
+    @options[:association] || @name
+  end
+
+  def association_class
+    association = @core.model.reflect_on_association(association_name)
+    association.klass
+  end
+end
